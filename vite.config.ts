b6ecb3bc432d559e7c3595b5f6311b5f112d6211
pereguda.vo <Vlad.rabota.96@yandex.ru>
2025-08-31 22:05:40 +0300

    test commits

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: '/test-vite',
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+			},
+		},
+	},
 })
